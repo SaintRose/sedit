1346862a676612cf7b2e0fd5252a57cf2ff1c3e4
Piotr Sedek <piotrseed@gmail.com>
2018-08-23 10:49:04 +0200

    pierwsza wersja

---
+++ assets/js/googlemap.js
@@ -0,0 +1,27 @@
+initMap();
+
+function initMap() {
+  var myLatLng = {
+    lat: 50.060930,
+    lng: 21.953305
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng,                        // Set the zoom level manually
+	zoomControl: true,
+	scaleControl: true,
+	scrollwheel: false,
+	disableDoubleClickZoom: true
+  });
+  var image = 'http://localhost/wp-content/themes/arhen/assets/images/marker.png';
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
+
+
+  google.maps.event.addListener(map, 'click', function(event){
+    this.setOptions({scrollwheel:true});
+  });
+}
