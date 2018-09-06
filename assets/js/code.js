df4d83a45a935128acafbaa58393cf87926ee739
PiotrSedek <piotrseed@gmail.com>
2018-09-06 23:34:58 +0200

    wersja stabilna

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,66 +1,13 @@
 jQuery(document).ready(function($) {
-  $("#top-slider").responsiveSlides({
-    pager: true,
-    timeout: 4000,
-    nav: false,
-    prevText: "",
-    nextText: "",
-    speed: 400
-  });
-  $(".fa-bars").click(function() {
-    $(".menu").slideToggle(0);
+
+  new ClipboardJS('.copy');
+
+  $('.dashicons-welcome-widgets-menus').click(function() {
+    $('.fa-copy').slideToggle(0);
   });
 
+  $('.fa-copy').click(function() {
+    $(this).css('color', '#9ECD7C');
+  });
 
-
-
-
-
-  $('.responsive').slick({
-      // dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          // centerMode: true,
-
-        }
-
-      }, {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          dots: false,
-          infinite: true,
-
-        }
-
-
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          dots: false,
-          infinite: true,
-
-        }
-      }, {
-        breakpoint: 320,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: false,
-          infinite: true,
-          autoplay: true,
-          autoplaySpeed: 2000,
-        }
-      }]
-    });
 });
---
+++ tabsClass.php
@@ -0,0 +1,95 @@
+<?php
+require_once 'modulesClass.php';
+//namespace SEDIT\tabs;
+use SEDIT\atoms\seditAtoms;
+
+/**
+ * Module
+ */
+class seditTabs extends seditAtoms
+{
+
+	function __construct()
+	{
+		add_action('admin_menu', array($this, 'pageInit'));
+	}
+
+	function pageInit(){
+		$title_blog = get_bloginfo('title');
+		add_menu_page($title_blog, $title_blog, 'manage_options', 'ustawienia', array($this, 'pageTabs'), 'dashicons-welcome-widgets-menus' ,0 );
+	}
+	// Pobranie wszytkich danych
+	public function pageTabsData($data) {
+	    $this->tabs =  $data;
+	}
+
+	public function pageTabs(){
+		if( isset( $_GET[ 'tab' ] ) ) {
+			$active_tab = $_GET[ 'tab' ];
+		}
+		?>
+		<div class="wrap">
+      <div id="icon-themes" class="icon32"></div>
+      <h2><span style="margin:6px 10px 0 0;" class="dashicons dashicons-welcome-widgets-menus"></span><?php bloginfo('title'); ?> - opcje dodatkowe</h2>
+			<?php settings_errors(); ?>
+			<div class="nav-tab-wrapper">
+				<?php
+					foreach ($this->tabs as $key => $value) {
+						echo '<a href="?page=ustawienia&tab='.$key.'" class="nav-tab '.($active_tab == $key ? 'nav-tab-active' : '').'">'.$key.'</a>';
+					}
+				 ?>
+			</div>
+			<form method="post" action="">
+				<table class="form-table">
+					<tbody>
+					<?php
+					foreach ($this->tabs as $key => $value) {
+						if ($_GET['tab'] === $key) {
+							foreach ($value as $title => $option) {
+								$text = null;
+								switch ($option['type']) {
+									case 'input':
+										echo seditAtoms::atomInput($title, $option);
+										break;
+									case 'textarea':
+										echo seditAtoms::atomTextarea($title, $option);
+										break;
+									case 'wpeditor':
+										echo seditAtoms::atomWpeditor($title, $option);
+										break;
+									case 'image':
+										echo seditAtoms::atomImage($title, $option);
+										break;
+									case 'title':
+										echo seditAtoms::atomTitle($title, $option);
+										break;
+									case 'separator':
+										echo seditAtoms::atomSeparator($title, $option);
+										break;
+									case 'module:google':
+										echo seditModules::moduleGoogle($title);
+										break;
+									default:
+										echo seditAtoms::atomError($title, $option);
+									break;
+								}
+								echo $text;
+							}
+						}
+					}
+					 ?>
+					 <tr>
+						<td>
+							<input name="submit" id="submit" class="button button-primary" value="Zapisz zmiany" type="submit">
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
+	 </div>
+		<?php
+	}
+
+
+
+}
