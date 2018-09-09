522a853e573a5875468cfa8a59d9be751661a2d4
PiotrSedek <piotrseed@gmail.com>
2018-09-09 23:47:46 +0200

    poprawka

--- tabsClass.php
+++ tabsClass.php
@@ -31,7 +31,7 @@ class seditTabs extends seditAtoms
 		<div class="wrap">
       <div id="icon-themes" class="icon32"></div>
       <h2><span style="margin:6px 10px 0 0;" class="dashicons dashicons-welcome-widgets-menus"></span><?php bloginfo('title'); ?> - opcje dodatkowe</h2>
-			<?php settings_errors(); ?>
+			<?php //settings_errors(); ?>
 			<div class="nav-tab-wrapper">
 				<?php
 					foreach ($this->tabs as $key => $value) {
