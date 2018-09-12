6f77f5d659d95c1b8b7f17b8dba559a64d5a9980
PiotrSedek <piotrseed@gmail.com>
2018-09-12 22:23:20 +0200

    pliki pdf dla zalogowanych

--- _functions.php
+++ _functions.php
@@ -7,3 +7,45 @@ $sedit->addSlider();
 register_nav_menus( array(
 	'top'    => 'Menu główne'
 ) );
+
+
+//do skasowania
+
+
+function login_pdf( $atts, $content ) {
+    extract( shortcode_atts( array(
+        'nazwa' => '',
+        'link' => '',
+    ), $atts ) );
+
+		if (is_user_logged_in()) {
+			$link = '<a href="'.$atts['link'].'">'.$atts['nazwa'].'</a>';
+		}else{
+			$link = $atts['nazwa'].' (<a href="'.wp_login_url().'">Zaloguj się, aby zobaczyć </a>)';
+		}
+
+    return $link;
+}
+add_shortcode( 'PDF', 'login_pdf' );
+
+// add new buttons
+add_filter( 'mce_buttons', 'myplugin_register_buttons' );
+function myplugin_register_buttons( $buttons ) {
+   array_push( $buttons, 'separator', 'myplugin' );
+   return $buttons;
+}
+add_action('init', 'wpse72394_shortcode_button_init');
+function wpse72394_shortcode_button_init() {
+		 if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') && get_user_option('rich_editing') == 'true')
+					return;
+		 add_filter("mce_external_plugins", "wpse72394_register_tinymce_plugin");
+		 add_filter('mce_buttons', 'wpse72394_add_tinymce_button');
+}
+function wpse72394_register_tinymce_plugin($plugin_array) {
+	 $plugin_array['wpse72394_button'] = 'path/to/shortcode.js';
+	 return $plugin_array;
+}
+function wpse72394_add_tinymce_button($buttons) {
+	 $buttons[] = "wpse72394_button";
+	 return $buttons;
+}
--- assets/js/code.js
+++ assets/js/code.js
@@ -10,4 +10,25 @@ jQuery(document).ready(function($) {
     $(this).css('color', '#9ECD7C');
   });
 
+
+
+  tinymce.create('tinymce.plugins.wpse72394_plugin', {
+          init : function(ed, url) {
+                  ed.addCommand('wpse72394_insert_shortcode', function() {
+                      selected = tinyMCE.activeEditor.selection.getContent();
+
+                      if( selected ){
+                          content =  '[PDF nazwa="'+selected+'" link="xxx"]';
+                      }else{
+                          content =  '[PDF nazwa="Nazwa linku" link="xxx"]';
+                      }
+
+                      tinymce.execCommand('mceInsertContent', false, content);
+                  });
+              ed.addButton('wpse72394_button', {title : 'PDF tylko dla zalogowanych', cmd : 'wpse72394_insert_shortcode' });
+          },
+      });
+      tinymce.PluginManager.add('wpse72394_button', tinymce.plugins.wpse72394_plugin);
+
+
 });
